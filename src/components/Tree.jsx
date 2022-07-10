6e78c1bb7fe8e867b590e989121c593818c5dfbf
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-07-10 12:50:25 -0500

    adds recurtion

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -3,15 +3,21 @@ import React, { useState } from 'react';
 const Tree = ({ treeData }) => {
   const [expand, setExpand] = useState(false);
 
-  return (
+  return treeData.isFolder ? (
     <div>
-      <span onClick={() => setExpand(true)}>{treeData.name}</span>
-      <div>
+      <span onClick={() => setExpand(!expand)}>
+        {treeData.name} <br />
+      </span>
+      <div style={{ display: expand ? 'block' : 'none', paddingLeft: 15 }}>
         {treeData.items.map((element) => {
-          return <span>element.name</span>;
+          return <Tree key={element.name} treeData={element} />;
         })}
       </div>
     </div>
+  ) : (
+    <span>
+      {treeData.name} <br />
+    </span>
   );
 };
 
