efeb80659e51116abe53a8d038482f12f322f551
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-07-10 12:00:37 -0500

    mapping tree items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Tree from './components/Tree';
+import treeData from './data/tree';
 
 function App() {
   return (
     <div className='App'>
-      <Tree />
+      <Tree treeData={treeData} />
     </div>
   );
 }
--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Tree = () => {
-  return <div>Tree</div>;
+const Tree = ({ treeData }) => {
+  const [expand, setExpand] = useState(false);
+
+  return (
+    <div>
+      <span onClick={() => setExpand(true)}>{treeData.name}</span>
+      <div>
+        {treeData.items.map((element) => {
+          return <span>element.name</span>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Tree;
